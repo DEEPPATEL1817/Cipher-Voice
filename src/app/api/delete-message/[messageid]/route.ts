641cb6ab5448cfdb9e355f66ff7c95943fadbcb7
DEEP PATEL <deep.patel8155@gmail.com>
2025-04-13 05:34:06 +0530

    error solve

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -3,6 +3,7 @@ import { authOptions } from "../../auth/[...nextauth]/options";
 import dbConnect from "@/lib/dbConnection";
 import UserModel from "@/models/user";
 import { User } from "next-auth";
+import { NextResponse } from 'next/server';
 
 
 
@@ -14,7 +15,7 @@ export async function DELETE(request : Request , {params}: {params:{messageid: s
     const user: User = session?.user as User
 
     if (!session || !session.user) {
-        return Response.json({
+        return NextResponse.json({
             success: false,
             message:"Not authenticated"
         },{status: 401})
@@ -26,20 +27,20 @@ export async function DELETE(request : Request , {params}: {params:{messageid: s
             {$pull: {messages: {_id: messageid}}}
         )
         if (updateResult.modifiedCount == 0) {
-            return Response.json({
+            return NextResponse.json({
                 success:false,
                 message: "Message not found or already Deleted"
             },{status: 404})
         }
 
-        return Response.json(
+        return NextResponse.json(
             {
                 success: true,
                 message : " messsage deleted"
             },{status: 200}
         )
     } catch (error) {
-        return Response.json(
+        return NextResponse.json(
             {
                 success: false,
                 message : "Error deleting message",error
