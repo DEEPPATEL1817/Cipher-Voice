334955bc0a19e405e609dab889388f7c440f1586
DEEP PATEL <deep.patel8155@gmail.com>
2025-04-24 18:10:24 +0530

    .

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -35,13 +35,8 @@ const Home = () => {
           </p>
         </section>
 
-        <div className="flex flex-col-reverse md:flex-row justify-between items-center gap-16">
-          <div className="left md:w-1/2">
-            <p className="flex items-center gap-2"><Check /> {`Truth hits harder when it's anonymous.`}</p>
-            <p className="flex items-center gap-2 text-seagreen"><Check /> No egos. No names. Just raw, honest feedback.</p>
-            <p className="flex items-center gap-2"><Check /> {`Because real change starts with what's real.`}</p>
-          </div>
-          <div className="right md:w-1/2 flex justify-center">
+        <div className="flex flex-col-reverse md:flex-row justify-between mx-20 items-center gap-40">
+          <div className="right md:w-1/2 flex justify-center p-12">
             <Image
               src="/anonymous-icone-1.jpg"
               alt="Anonymous feedback icon"
@@ -50,43 +45,68 @@ const Home = () => {
               className="max-w-full h-auto md:max-w-md opacity-80 hover:opacity-100 transition-opacity"
             />
           </div>
+          <div className="left md:w-1/2" >
+            <p className="flex items-center gap-2 pb-2"><Check className='bg-green-400' /> {`Truth hits harder when it's anonymous.`}</p>
+            <p className="flex items-center gap-2 pb-2"><Check className='bg-green-400' /> No egos. No names. Just raw, honest feedback.</p>
+            <p className="flex items-center gap-2 pb-2"><Check className='bg-green-400' /> {`Because real change starts with what's real.`}</p>
+          </div>
         </div>
 
 
-        <Carousel
-          plugins={[Autoplay({ delay: 2000 })]}
-          className="w-full max-w-xs mt-32 " >
-          <CarouselContent>
-            {
-              messages.map((messages, index) => (
-                <CarouselItem key={index}>
-                  <div className="p-1">
-                    <Card >
-                      <CardHeader>
-                        {messages.title}
-                      </CardHeader>
-                      <CardContent className="flex aspect-square items-center justify-center p-6">
-                        <span className="text-lg font-semibold">{messages.content}</span>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))
-            }
-          </CarouselContent>
-          <CarouselPrevious className='text-gray-500' />
-          <CarouselNext className='text-gray-500' />
-        </Carousel>
+        <div className='flex flex-col-reverse md:flex-row justify-between gap-40 items-center mx-20'>
+        <div className="flex flex-col w-full md:w-1/2">
+          <p className='mb-3' >{`Everyone has something to say - an appreciation, a suggestion, or even a hard truth.
+            Our platform gives people the space to share honest feedback without fear or filters.
+            Whether it's for a friend, a teammate, or a stranger, your words have power.
+            Help others grow, reflect, and improve - one message at a time.
+            It’s simple, anonymous, and made to spark real conversations.
+            Because growth starts with honest feedback.`} </p><br />
+           <p>{` Every opinion fuels our next move.
+            Your words inspire us to grow, improve, and innovate.<br />
+            These stories aren't just feedback—
+            they're proof that we’re on the right track.`}
+          </p>
+          </div>
+          <Carousel
+            plugins={[Autoplay({ delay: 2000 })]}
+            className="w-full max-w-xs mt-32 " >
+            <CarouselContent>
+              {
+                messages.map((messages, index) => (
+                  <CarouselItem key={index}>
+                    <div className="p-1">
+                      <Card >
+                        <CardHeader>
+                          {messages.title}
+                        </CardHeader>
+                        <CardContent className="flex aspect-square items-center justify-center p-6">
+                          <span className="text-lg font-semibold">{messages.content}</span>
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))
+              }
+            </CarouselContent>
+            <CarouselPrevious className='text-gray-500' />
+            <CarouselNext className='text-gray-500' />
+          </Carousel>
+        </div>
 
 
-        <Accordion type="single" collapsible className="w-full mt-10 md:w-96">
-          {FAQ.map((faq, index) => (
-            <AccordionItem key={index} value={`item-${index}`}>
-              <AccordionTrigger>{faq.Question}</AccordionTrigger>
-              <AccordionContent>{faq.Answer}</AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
+        <div className='mt-12 text-3xl '>
+          <div className='flex justify-center items-center underline font-bold'>
+            {`FAQ's`}
+          </div>
+          <Accordion type="single" collapsible className="w-full md:w-7xl mt-2 pl-1.5 ">
+            {FAQ.map((faq, index) => (
+              <AccordionItem key={index} value={`item-${index}`}>
+                <AccordionTrigger>{faq.Question}</AccordionTrigger>
+                <AccordionContent>{faq.Answer}</AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
 
       </main>
       <footer className='text-center p-4 md:p-6'>@ 2025 Cipher Voice. All rights reserved</footer>
